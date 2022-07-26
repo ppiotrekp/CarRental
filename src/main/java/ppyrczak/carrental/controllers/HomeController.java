57ea533f6989ff8b0411fd0bc8c77cdc55ca865f
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-07-27 00:57:56 +0200

    home page and register done

---
+++ src/main/java/ppyrczak/carrental/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package ppyrczak.carrental.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String showHomePage() {
+        return "home";
+    }
+}
--- src/main/java/ppyrczak/carrental/entities/User.java
+++ src/main/java/ppyrczak/carrental/entities/User.java
@@ -15,14 +15,38 @@ public class User {
     @NotBlank(message = "Name is mandatory")
     private String name;
 
+    @NotBlank(message = "Surname is mandatory")
+    private String surname;
+
     @NotBlank(message = "Email is mandatory")
     private String email;
 
+    @NotBlank(message = "Phone number is mandatory")
+    private String phoneNumber;
+
+    @NotBlank(message = "City is mandatory")
+    private String city;
+
+    @NotBlank(message = "Street is mandatory")
+    private String street;
+
+    @NotBlank(message = "Home number is mandatory")
+    private String homeNumber;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
     public User() {}
 
-    public User(String name, String email) {
+    public User(String name, String email, String surname, String phoneNumber, String city, String street, String homeNumber, String password) {
         this.name = name;
         this.email = email;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.street = street;
+        this.homeNumber = homeNumber;
+        this.password = password;
     }
 
     public void setId(long id) {
@@ -47,6 +71,58 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHomeNumber() {
+        return homeNumber;
+    }
+
+    public void setHomeNumber(String homeNumber) {
+        this.homeNumber = homeNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean userIdentification(String email, String password) {
+        return true;
     }
 
     @Override
