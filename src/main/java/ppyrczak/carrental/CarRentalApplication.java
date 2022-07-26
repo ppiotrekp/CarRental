2087cabb4ef02e6bdbade5f0f21407f7dbe5a803
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-07-26 22:30:40 +0200

    initial commit

---
+++ src/main/java/ppyrczak/carrental/CarRentalApplication.java
@@ -0,0 +1,14 @@
+package ppyrczak.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
+@SpringBootApplication
+public class CarRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ppyrczak/carrental/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package ppyrczak.carrental.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ppyrczak.carrental.entities.User;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByName(String name);
+
+}
