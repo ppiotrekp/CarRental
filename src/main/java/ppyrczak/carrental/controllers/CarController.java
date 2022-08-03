6ae14e1a22f947c940e41e47e55fdfab712901f6
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-03 15:52:00 +0200

    showRentPanel corrected to see distinct id

--- src/main/java/ppyrczak/carrental/controllers/CarController.java
+++ src/main/java/ppyrczak/carrental/controllers/CarController.java
@@ -2,34 +2,45 @@ package ppyrczak.carrental.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import ppyrczak.carrental.entities.Car;
+import ppyrczak.carrental.entities.User;
 import ppyrczak.carrental.repositories.CarRepository;
+import ppyrczak.carrental.repositories.UserRepository;
 import ppyrczak.carrental.services.CarService;
-import ppyrczak.carrental.utils.Fuel;
-import ppyrczak.carrental.utils.Transmission;
+
+import java.util.Optional;
 
 //@Data
 @Controller
 @AllArgsConstructor
 @Slf4j
+
 public class CarController {
     private final CarService carService;
-    private final CarRepository carRepository;
 
+    private final CarRepository carRepository;
+    private final UserRepository userRepository;
 
     @GetMapping("/cars")
     public String showAvailableCars(Model model) {
-        log.info("cars: " + carService.getAllCars());
+        log.info("userid" + UserController.userId);
         model.addAttribute("cars", carService.getAllCars());
 
-        //model.addAttribute("fuel", Fuel.values());
-        //model.addAttribute("transmission", Transmission.values());
         return "cars";
+    }
+
+    @GetMapping("/rent-panel/{id}")
+    public String showRentPanel(@PathVariable Long id, Model model) {
+        Car car = carRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid car Id:" + id));
+        Optional<User> user = userRepository.findById(UserController.userId);
+        log.info("userid" + UserController.userId);
+        model.addAttribute("car", car);
+        model.addAttribute("user", user);
+        return "rent-panel";
     }
 
 
