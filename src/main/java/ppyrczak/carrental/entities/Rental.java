5971cf12f89a7e20d98b63e0b510c4720ad9e3a7
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-03 15:50:20 +0200

    rental entity joined with car and user entities

--- src/main/java/ppyrczak/carrental/entities/Rental.java
+++ src/main/java/ppyrczak/carrental/entities/Rental.java
@@ -1,24 +1,50 @@
 package ppyrczak.carrental.entities;
 
+import lombok.Data;
 import ppyrczak.carrental.utils.RentalSpan;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
+@Data
 public class Rental {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long userId;
-    private Long carId;
     private Date rentalStart;
     private Date rentalEnd;
     private RentalSpan rentalSpan;
     private int price;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id", referencedColumnName = "id")
+    private Car car;
+
+    public int getPrice() {
+        if (rentalSpan == RentalSpan.WEEKEND) {
+            price = car.getPriceForWeekend();
+        }
+
+        else if (rentalSpan == RentalSpan.WEEK) {
+            price = car.getPriceForWeek();
+        }
+
+        else if (rentalSpan == RentalSpan.TWO_WEEKS) {
+            price = car.getPriceForTwoWeeks();
+        }
+
+        else {
+            price = car.getPriceForMonth();
+        }
+
+        return price;
+    }
 
 
 
