94280969ed6401b90f85ef4f6b3535e7f6f67912
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-05 00:06:39 +0200

    Enum converted to string

--- src/main/java/ppyrczak/carrental/utils/RentalSpan.java
+++ src/main/java/ppyrczak/carrental/utils/RentalSpan.java
@@ -1,5 +1,18 @@
 package ppyrczak.carrental.utils;
 
 public enum RentalSpan {
-    WEEKEND, WEEK, TWO_WEEKS, MONTH
+    WEEKEND("Weekend"),
+    WEEK("Week"),
+    TWO_WEEKS("Two weeks"),
+    MONTH("Month");
+
+    private final String rentalSpan;
+
+    RentalSpan(String rentalSpan) {
+        this.rentalSpan = rentalSpan;
+    }
+
+    public String toString() {
+        return this.rentalSpan;
+    }
 }
