04197b3c3a9f674f50d593198e349d09f0cd40f0
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-08 16:31:37 +0200

    new method with a query annotation

--- src/main/java/ppyrczak/carrental/repositories/CarRepository.java
+++ src/main/java/ppyrczak/carrental/repositories/CarRepository.java
@@ -1,13 +1,18 @@
 package ppyrczak.carrental.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ppyrczak.carrental.entities.Car;
-import ppyrczak.carrental.entities.User;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
     Car findById(Car car);
 
     Car findOnlyById(Long carId);
+
+    @Query(value = "select c.id, c.brand, c.model, c.fuel, c.transmission, c.power from car c" , nativeQuery = true)
+    List<Car> getAllCars();
+
+
 }
