6de128659a1ae8b578a051281c437d31fc20aa25
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-09 15:21:55 +0200

    logged variable used

--- src/main/java/ppyrczak/carrental/controllers/UserController.java
+++ src/main/java/ppyrczak/carrental/controllers/UserController.java
@@ -6,16 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import ppyrczak.carrental.entities.Car;
 import ppyrczak.carrental.entities.User;
 import ppyrczak.carrental.repositories.CarRepository;
 import ppyrczak.carrental.repositories.UserRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @Slf4j
@@ -24,6 +23,8 @@ public class UserController {
     private final UserRepository userRepository;
     private final CarRepository carRepository;
     public static Long userId;
+
+    private boolean logged = false;
 
     @Autowired
     public UserController(UserRepository userRepository, CarRepository carRepository) {
@@ -38,18 +39,19 @@ public class UserController {
     }
 
     @GetMapping("/signup")
-    public String showSignUpForm(User user) {
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
         return "add-user";
     }
 
     @PostMapping("/adduser")
-    public String addUser(@Valid User user, BindingResult result, Model model) {
+    public String addUser(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "add-user";
         }
-
+      //  model.addAttribute("user", user);
         userRepository.save(user);
-        return "redirect:/index";
+        return "redirect:/home";
     }
 
     @GetMapping("/signin")
@@ -58,26 +60,29 @@ public class UserController {
     }
 
     @GetMapping("/panel-user")
-    public String identifyUser(String email, User user, String password, Model model, HttpServletRequest request) {
-            if (userRepository.existsByEmailAndPassword(email, password)) {
-                user = userRepository.findByEmailAndPassword(email, password);
-                model.addAttribute("user", user);
-                userId = user.getId();
-                log.info("Works" + userId.toString());
-                return "panel-user";
-            }
+    public String identifyUser(String email, User user, String password, Model model) {
 
-            else {
-                log.info("Wrong email or password");
-                return "redirect:/signin";
-            }
+        if (userRepository.existsByEmailAndPassword(email, password)) {
+            logged = true;
+            user = userRepository.findByEmailAndPassword(email, password);
+            model.addAttribute("user", user);
+            userId = user.getId();
+        }
+
+        if (logged = true) {
+            return "panel-user";
+        }
+
+        else {
+            log.info("Wrong email or password");
+            return "redirect:/signin";
+        }
     }
 
     @GetMapping("/update-user/{id}")
     public String showUpdateForm(@PathVariable("id") Long id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         model.addAttribute("user", user);
-
         return "update-user";
     }
 
@@ -89,7 +94,6 @@ public class UserController {
         }
 
         userRepository.save(user);
-
         return "redirect:/panel-user";
     }
 
@@ -97,7 +101,21 @@ public class UserController {
     public String deleteUser(@PathVariable("id") Long id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         userRepository.delete(user);
-
         return "redirect:/index";
+    }
+
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String showContactPage() {
+        return "contact";
+    }
+
+    @GetMapping("/home")
+    public String showHomePage() {
+        return "home";
     }
 }
