52797c9df9a27d6c74ab158a6884d4d834c96218
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-08-10 11:43:18 +0200

    constraints for email and phone number done

--- src/main/java/ppyrczak/carrental/entities/User.java
+++ src/main/java/ppyrczak/carrental/entities/User.java
@@ -1,9 +1,10 @@
 package ppyrczak.carrental.entities;
 
 import lombok.Data;
-
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,8 +22,15 @@ public class User {
     private String surname;
 
     @NotBlank(message = "Email is mandatory")
+    @Email(message = "email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\" +
+            ".[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\" +
+            "x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\" +
+            ".)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\" +
+            ".){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f" +
+            "\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     private String email;
 
+    @Pattern(message = "Invalid phone number", regexp = "^([1-9][0-9]{8})$")
     @NotBlank(message = "Phone number is mandatory")
     private String phoneNumber;
 
@@ -44,7 +52,4 @@ public class User {
             fetch = FetchType.LAZY
     )
     private List<Rental> rentals = new ArrayList<>();
-
-
-
 }
